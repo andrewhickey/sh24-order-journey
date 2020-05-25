2ceae9529ca4ceb972e070b6beb2f3c4ea5ddc53
Andrew Hickey <andrewhickey@live.co.uk>
2020-05-25 14:25:38 +0100

    style: shifts edit buttons to the right

--- src/pages/SummaryPage/SummaryPage.tsx
+++ src/pages/SummaryPage/SummaryPage.tsx
@@ -9,22 +9,28 @@ type SummaryPageProps = {
 function SummaryPage({ userInput }: SummaryPageProps) {
   return (
     <>
-      <h1 className="text-xl mb-6">Summary</h1>
+      <h1 className="text-xl font-bold mb-6 space-y-4">Summary</h1>
       <div>
-        <div>
-          Name: {userInput.name}{' '}
+        <div className="flex justify-between">
+          <span>
+            Name: <span className="font-bold">{userInput.name} </span>
+          </span>
           <Link to="/" className="text-red-300">
             edit
           </Link>
         </div>
-        <div>
-          E-Mail: {userInput.email}{' '}
+        <div className="flex justify-between">
+          <span>
+            E-Mail: <span className="font-bold">{userInput.email} </span>
+          </span>
           <Link to="/email" className="text-red-300">
             edit
           </Link>
         </div>
-        <div>
-          Service: {userInput.service}{' '}
+        <div className="flex justify-between">
+          <span>
+            Service: <span className="font-bold">{userInput.service}</span>
+          </span>
           <Link to="/service" className="text-red-300">
             edit
           </Link>
